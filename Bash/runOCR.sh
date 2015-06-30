bea10ce56df49dbae8be8aec5cdb172384d415f8
Carlos <carloscaicedos@gmail.com>
2015-06-29 22:13:37 -0500

    El script crea carpeta para la salida hocr

--- Bash/runOCR.sh
+++ Bash/runOCR.sh
@@ -2,9 +2,15 @@
 #Get text from images with Tesseract OCR
 
 cd $1
+
+if [ ! -d "hocr" ]; then
+mkdir hocr
+echo "hocr folder created!"
+fi
+
 FILES=$(find ./ -type f)
 for f in $FILES
 do
 echo $f
-tesseract $f $f -l spa hocr
+tesseract $f ./hocr/$f -l spa hocr
 done
